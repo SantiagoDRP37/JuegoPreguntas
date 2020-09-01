cc810802e6951c56a861ece49dd3a0ae0ac46dd4
SantiagoRojasPc <santiagodrp37@gmail.com>
2020-08-31 20:29:29 -0500

    avance

--- script/mainScript.js
+++ script/mainScript.js
@@ -15,6 +15,7 @@ const apiPreguntas = 'https://opentdb.com/api.php?amount=10&difficulty=hard&type
 };
 
 let preguntasBD = [];
+let respuestas = [];
 function iniciarJuego(preguntas){
     preguntasBD = preguntas;
     dibujarPreguntas()
@@ -22,15 +23,50 @@ function iniciarJuego(preguntas){
 function dibujarPreguntas(){
     const cabeza = document.getElementById("tituloConte");
     const cuerpo = document.getElementById("cuerpo")
+
     let titulo = document.createElement("h1")
         titulo.setAttribute("id", "titulo");
         titulo.textContent = preguntasBD[0].category;
-    let Pregunta =  document.createElement("h1")
+    let Pregunta =  document.createElement("h2")
         Pregunta.setAttribute("id", "pregunta");
         Pregunta.textContent = preguntasBD[0].question;
-    
+    let botonTrue = document.createElement("button")
+        botonTrue.setAttribute("id", "botonTrue");
+        botonTrue.innerHTML = "True"
+    let botonFalse = document.createElement("button")
+    botonFalse.setAttribute("id", "botonFalse");
+    botonFalse.innerHTML = "False"
+
         cabeza.appendChild(titulo);
         cuerpo.appendChild(Pregunta);
+        cuerpo.appendChild(botonTrue);
+        cuerpo.appendChild(botonFalse);
+        eventosRespuesta()
+    
+}
+function eventosRespuesta(){
+    let eventoVerdad = document.getElementById("botonTrue");
+    eventoVerdad.addEventListener('click',()=>{
+        respuestas.push("True");
+        
+        cambiarPregunta()
+    });
+    let eventoFalso = document.getElementById("botonFalse");
+    eventoFalso.addEventListener('click',()=>{
+        respuestas.push("False");
+        cambiarPregunta()
+    });
+}
+let i = 0;
+function cambiarPregunta(){
+    i = i+ 1;
+    if(respuestas.length <10){
+        
+        console.log("i = "+i);
+        const pregunta = document.getElementById("pregunta");
+        pregunta.textContent = preguntasBD[i].question;
+    }
+    
 
 }
 function ocultarElementos(elementoEle){
@@ -45,6 +81,5 @@ const comenzarBoton = document.getElementById("botonComenzar");
 comenzarBoton.addEventListener('click',()=>{
    pregunta();
    ocultarElementos('h2');
-   ocultarElementos('h1');
-   
+   ocultarElementos('h1'); 
 });
